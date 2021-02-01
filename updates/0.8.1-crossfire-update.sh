635f740c3376849364db189723a04e6c6fa94b8f
crypto-guys <50590698+crypto-guys@users.noreply.github.com>
2021-02-01 03:35:33 -0600

    Create 0.8.1-crossfire-update.sh

---
+++ updates/0.8.1-crossfire-update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Retrieve the software
+mkdir -p ~/crossfire
+wget https://github.com/crypto-com/chain-main/releases/download/v0.8.1-crossfire/chain-main_0.8.1-crossfire_Linux_x86_64.tar.gz -O ~/crossfire
+tar -zxvf ~/crossfire/chain-main_0.8.1-crossfire_Linux_x86_64.tar.gz && cd chain-main_0.8.1-crossfire_Linux_x86_64
+
+
+# Stop the running service
+sudo systemctl stop chain-maind.service
+
+# Update the binary
+sudo cp chain-maind /usr/local/bin/
+
+# Check version
+VERSION=$(chain-maind version)
+if [ $VERSION != "0.8.1-crossfire" ]
+then
+echo "The binary file is not the expected version it is reporting version: $VERSION"
+echo "Exiting"
+quit
+
+# Remove the address book
+rm ~/.chain-maind/config/addrbook.json
+
+# Increase your memory pool size:   
+# File location: ~/.chain-maind/config/config.toml
+# Setting
+# Maximum number of transactions in the mempool
+size = 20000
+
+# Start the service with the new files and settings
+sudo systemctl start chain-maind.service
